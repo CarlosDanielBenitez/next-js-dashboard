e3a9c1b3fd30b00c15fa157b108c1d57e6f20166
CarlosDanielBenitez <cdanibenz@gmail.com>
2023-12-07 17:24:37 -0300

    Features: implement sql in my project

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,9 @@
-export default function DashboardPage(){
+import { log } from "console"
+import { fetchRevenue } from "../lib/data"
+
+export default async function DashboardPage(){
+    const revenue = await fetchRevenue()
+    console.log(revenue);
+    
     return <p>Dashboard Page</p>
 }
